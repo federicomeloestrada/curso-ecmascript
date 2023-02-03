a31c178610fc09f547984d8f5a14c1f55c464cd5
federicomeloestrada <federicomeloestrada@hotmail.com>
2023-02-03 12:11:43 -0500

    Primeros archivos curso ecmascript

---
+++ src/es6/01-arrow-function.js
@@ -0,0 +1,13 @@
+function square1(num) {
+    return num * num;
+}
+
+const square2 = (num) => {
+    return num * num;
+}
+
+const square3 = num => num * num;
+
+console.log(square1(2));
+console.log(square2(2));
+console.log(square3(2));
